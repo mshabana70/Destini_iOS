61149d56320e9f0c459bcb16d28c92a3968328ec
Mahmoud Shabana <mshabana5656@gmail.com>
2020-06-20 03:20:42 -0400

    Completed challenge

--- Destini-iOS13/AppDelegate.swift
+++ Destini-iOS13/AppDelegate.swift
@@ -3,6 +3,7 @@
 //  Destini-iOS13
 //
 //  Created by Angela Yu on 08/08/2019.
+//  Modified by Mahmoud Shabana on 06/19/2020
 //  Copyright © 2019 The App Brewery. All rights reserved.
 //
 
--- Destini-iOS13/Model/Story.swift
+++ Destini-iOS13/Model/Story.swift
@@ -1,5 +1,5 @@
 //
-//  Sotry.swift
+//  Story.swift
 //  Destini-iOS13
 //
 //  Created by Angela Yu on 08/08/2019.
@@ -8,3 +8,12 @@
 //
 
 import Foundation
+
+struct Story {
+    let title: String
+    let choice1: String
+    let choice1Destination: Int
+    let choice2: String
+    let choice2Destination: Int
+    
+}
